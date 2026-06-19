d5e06cbf0e7b98a70b2e5d2a9a0e88ecc22fc480
Dat Tran <127409761+drxaviert@users.noreply.github.com>
2026-06-19 16:23:57 -0400

    Feat: Add a button asking for grid's size

--- script/script.js
+++ script/script.js
@@ -1,14 +1,32 @@
+let numBlock = 4;
+
+const input = document.querySelector("#input");
 const gridContainer = document.querySelector("#grid-container");
 
-let num = 4;
-let blockSize = `${100 / num}%`;
+function createGrid() {
+  let blockSize = `${100 / numBlock}%`;
 
-for (let i = 0; i < num ** 2; i++) {
-  const block = document.createElement("div");
-  block.classList.add("block");
+  for (let i = 0; i < numBlock ** 2; i++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
 
-  block.style.blockSize = blockSize;
-  block.style.inlineSize = blockSize;
+    block.style.blockSize = blockSize;
+    block.style.inlineSize = blockSize;
 
-  gridContainer.appendChild(block);
+    gridContainer.appendChild(block);
+  }
 }
+
+function askNumBlock() {
+  return (numBlock = Math.min(
+    +prompt("How big do you want your grid to be?", 4),
+    100,
+  ));
+}
+
+createGrid();
+
+input.addEventListener("click", (e) => {
+  askNumBlock();
+  createGrid();
+});
