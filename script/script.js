bdb8b110151103b776544a6a54a47f32f0c95ebf
Dat Tran <127409761+drxaviert@users.noreply.github.com>
2026-06-18 23:10:41 -0400

    Create boilerplate for project

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const gridContainer = document.querySelector("#gridContainer");
+
+const block = document.createElement("div");
+
+block.classList.add("block");
+block.textContent = "gei";
+
+gridContainer.appendChild(block);
