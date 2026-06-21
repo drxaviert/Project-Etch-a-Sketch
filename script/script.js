c283f7f5895453d8ceb7f15efa5330d56329ba7f
Dat Tran <127409761+drxaviert@users.noreply.github.com>
2026-06-21 17:34:30 -0400

    refactor: Refactor the logic for practice

--- script/script.js
+++ script/script.js
@@ -1,14 +1,56 @@
-let numBlock = 4;
-let blockSize = `${100 / numBlock}%`;
+let numBlock = 2;
 
 const gridContainer = document.querySelector("#grid-container");
 
-for (let i = 0; i < numBlock ** 2; i++) {
-  const block = document.createElement("div");
+const buttonSize = document.querySelector("#button-size");
 
-  block.classList.toggle("block-static");
-  block.style.blockSize = blockSize;
-  block.style.inlineSize = blockSize;
-
-  gridContainer.appendChild(block);
+function resetGrid() {
+  const blocks = document.querySelectorAll(".block-static");
+  blocks.forEach((block) => block.remove());
 }
+
+function interaction() {
+  const blocks = document.querySelectorAll(".block-static");
+  blocks.forEach((block) => {
+    let opacity = 1;
+
+    block.addEventListener("mouseenter", () => {
+      let r = Math.random() * 255;
+      let g = Math.random() * 255;
+      let b = Math.random() * 255;
+      let randColor = `rgb(${r},${g},${b}`;
+
+      opacity -= 0.1;
+
+      block.style.backgroundColor = randColor;
+      block.style.opacity = opacity;
+    });
+  });
+}
+
+function createGrid() {
+  resetGrid();
+
+  let blockSize = `${100 / numBlock}%`;
+
+  for (let i = 0; i < numBlock ** 2; i++) {
+    const block = document.createElement("div");
+
+    block.classList.toggle("block-static");
+    block.style.blockSize = blockSize;
+    block.style.inlineSize = blockSize;
+
+    gridContainer.appendChild(block);
+  }
+
+  interaction();
+}
+
+createGrid();
+
+buttonSize.addEventListener("click", () => {
+  numBlock = prompt("How big do you want your grid to be?", 4);
+  numBlock = Math.min(numBlock, 5);
+
+  createGrid();
+});
